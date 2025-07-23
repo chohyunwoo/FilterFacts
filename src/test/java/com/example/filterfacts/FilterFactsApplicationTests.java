b6510a3fefb68afe6e7f5081824410f95a1f09e8
조현우 <gusdndlek12@naver.com>
2025-07-23 19:58:41 +0900

    first

---
+++ src/test/java/com/example/filterfacts/FilterFactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filterfacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilterFactsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
