028ed23264f61bf6e5f15250c624c5d43f32b853
조현우 <gusdndlek12@naver.com>
2025-07-27 19:59:40 +0900

    서브모듈 제거 및 일반 폴더로 재추가

---
+++ backend/src/main/java/com/example/filterfacts/FilterFactsApplication.java
@@ -0,0 +1,13 @@
+package com.example.filterfacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterFactsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilterFactsApplication.class, args);
+    }
+
+}
